7459b6d3b0bdc554efd9324969212d5dee5e3502
Hane Liu <hane0131@gmail.com>
2015-06-26 23:49:43 +0800

    Add login form

--- login.php
+++ login.php
@@ -10,6 +10,11 @@
     <title>管理員登入</title>
 </head>
 <body>
-
+    <form method="POST">
+        帳號：<input type="text" name="id"><br>
+        密碼：<input type="password" name="passwd"><br>
+        <input type="hidden" name="action" value="submit">
+        <input type="submit" value="登入">
+    </form>
 </body>
 </html>
