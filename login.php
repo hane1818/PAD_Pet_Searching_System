1218e6c55c73c76e9032f7a255cb93fd4e109176
Hane Liu <hane0131@gmail.com>
2015-06-26 23:25:38 +0800

    Justify session for admin

--- login.php
+++ login.php
@@ -1,3 +1,9 @@
+<?php
+    session_start();
+    //先判斷管理員是否登入，有登入就導向至管理系統
+    if(isset($_SESSION["admin"]))
+        echo '<meta http-equiv="refresh" content="0; url=manageSystem.php">';
+?>
 <!DOCTYPE html>
 <html>
 <head>
