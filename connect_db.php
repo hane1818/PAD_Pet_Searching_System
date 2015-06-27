c90ab3f7971c58c3cb6cc1da5303defebb81db7a
Hane Liu <hane0131@gmail.com>
2015-06-27 10:02:10 +0800

    Modify connection query

--- connect_db.php
+++ connect_db.php
@@ -12,7 +12,7 @@
     $db_passwd = $_ENV['PAD_PASSWD'];
     $db_port = $_ENV['PAD_DBPORT'];
 
-    $conn = pg_connect("pgsql:host=$db_server;port=$db_port;dbname=$db_name;user=$db_user;password=$db_passwd");
+    $conn = pg_connect("host=$db_server;port=$db_port;dbname=$db_name;user=$db_user;password=$db_passwd");
 
     if(!$conn)
         echo "Failed!";
