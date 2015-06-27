d4c2b3a55a1540471de6aff8bb975234282c919e
Hane Liu <hane0131@gmail.com>
2015-06-27 09:54:20 +0800

    Initial commit

---
+++ connect_db.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title></title>
+</head>
+<body>
+<?php
+    $db_server = $_ENV['PAD_HOST'];
+    $db_name = $_ENV['PAD_DBNAME'];
+    $db_user = $_ENV['PAD_USER'];
+    $db_passwd = $_ENV['PAD_PASSWD'];
+    $db_port = $_ENV['PAD_DBPORT'];
+?>
+</body>
+</html>
