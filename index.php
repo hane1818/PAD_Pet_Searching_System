a782f93e61f6ccf87553a2a9119fc783a8c1a836
Hane Liu <hane0131@gmail.com>
2015-06-28 19:24:56 +0800

    Modify content

--- index.php
+++ index.php
@@ -5,8 +5,26 @@
     <title>首頁</title>
 </head>
 <body>
-<?php
-    phpinfo();
-?>
+    <header>
+        <div>
+            <h1><a href="./">龍族拼圖寵物查詢系統</a></h1>
+        </div>
+        <nav>
+            <ul>
+                <li><a href="./">Home</a></li>
+                <li><a href="about.php">About</a></li>
+                <li><a href="#">Search pet</a></li>
+            </ul>
+        </nav>
+        <div>
+            <a href="login.php">Dashboard</a>
+        </div>
+    </header>
+    <div id="main">
+
+    </div>
+    <footer>
+        <p>&copy;劉慈恩、鄭瑜、林筠傑</p>
+    </footer>
 </body>
 </html>
