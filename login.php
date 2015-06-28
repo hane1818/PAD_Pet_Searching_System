7a954410fbc806fddf238e022bc359c454a5b3e8
Hane Liu <hane0131@gmail.com>
2015-06-28 22:24:20 +0800

    Modify content to display

--- login.php
+++ login.php
@@ -9,13 +9,33 @@
 <head>
     <meta charset="utf-8">
     <title>管理員登入</title>
+    <link rel="stylesheet" type="text/css" href="assets/stylesheets/main.css">
 </head>
 <body>
-    <form method="POST">
-        帳號：<input type="text" name="id"><br>
-        密碼：<input type="password" name="passwd"><br>
-        <input type="hidden" name="action" value="submit">
-        <input type="submit" value="登入">
-    </form>
+    <header>
+        <div class="wrapper">
+            <a id="title" href="./">龍族拼圖寵物查詢系統</a>
+            <nav>
+                <ul>
+                    <li><a href="./">Home</a></li>
+                    <li><a href="about.php">About</a></li>
+                    <li><a href="#">Search</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    <div id="main">
+        <div class="wrapper">
+            <form method="POST">
+                帳號：<input type="text" name="id"><br>
+                密碼：<input type="password" name="passwd"><br>
+                <input type="hidden" name="action" value="submit">
+                <input type="submit" value="登入">
+            </form>
+        </div>
+    </div>
+    <footer>
+        <p>&copy;2015, 劉慈恩、鄭瑜、林筠傑</p>
+    </footer>
 </body>
 </html>
