260a34e93a53c7faf1d09e8c673046074a3c71c0
Hane Liu <hane0131@gmail.com>
2015-06-29 16:17:31 +0800

    Add function popup()

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,14 @@
+function popup(id) {
+    if(document.getElementsByClassName('popup_back')[0])
+    {
+        document.getElementsByClassName('popup_back')[0].style.display = "block";
+        document.getElementsByClassName('popup_news')[0].style.display = "block";
+        document.getElementsByClassName('popup_news')[0].innerHTML = '<iframe src="pet.php?id='+id+'"></iframe>';
+    }
+    else
+        document.getElementById('main').innerHTML += '<div class="popup_back" onclick="popoff()" ><input type="submit" class="sure" onclick="popoff()" autofocus value="X"></div><div class="popup_news"><iframe src="pet.php?id='+id+'"></iframe></div>';
+}
+
 function popoff() {
     document.getElementsByClassName('popup_back')[0].style.display = "none";
     document.getElementsByClassName('popup_news')[0].style.display = "none";
