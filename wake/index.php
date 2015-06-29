2f2e8da48f948757a50b9ff67b2267ce754ee251
Hane Liu <hane0131@gmail.com>
2015-06-29 20:04:42 +0800

    Modify code perform

--- wake/index.php
+++ wake/index.php
@@ -9,15 +9,34 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>PAD寵物資料庫管理系統</title>
-		<meta charset="utf-8">
+	<meta charset="utf-8">
+	<title>龍族拼圖寵物查詢系統 - 管理後台</title>
+	<link rel="stylesheet" type="text/css" href="assets/stylesheets/main.css">
+    <script type="text/javascript" src="assets/scripts/main.js"></script>
 </head>
 <body>
-	<h1 align="center">覺醒技能一覽</h1>
-	<table style="border:5px #cccccc double;" align="center">
-		<tr><th>ID</th><th>ICON</th><th>技能名稱</th><th colspan="2">效果</th><th>Action</th></tr>
-		<tr>
-		<?php
+	<header>
+        <div class="wrapper">
+            <a id="title" href="dashboard.php">管理後台</a>
+            <nav>
+                <ul>
+                    <li><a href="../">Home</a></li>
+                    <li><a href="../">寵物</a></li>
+                    <li><a href="../wake/">覺醒技能</a></li>
+                    <li><a href="../askill/">主動技能</a></li>
+                    <li><a href="../lskill/">隊長技能</a></li>
+                </ul>
+            </nav>
+            <a id="logout" href="../logout.php">登出</a>
+        </div>
+    </header>
+    <div id="main">
+        <div class="wrapper">
+			<h1 align="center">覺醒技能一覽</h1>
+			<table style="border:5px #cccccc double;" align="center">
+				<tr><th>ID</th><th>ICON</th><th>技能名稱</th><th colspan="2">效果</th><th>Action</th></tr>
+				<tr>
+				<?php
 				//連接MySQL伺服器
 				$link_ID = mysql_connect('localhost', 'root', '123');
 
@@ -39,38 +58,39 @@
 						echo '<td><img src="'.$url[$i].'" width="25" align="absmiddle"></td>';
 						echo '<td>'.$name[$i].'</td>';
 						echo '<td>'.$effect[$i].'</td>';
-						echo '<td><a href="update.php?id='.$id[$i].'&url='.$url[$i].'&name='.$name[$i].'&effect='.$effect[$i].'">修改</td>';
+						echo '<td><a onclick="popup(&quot;update.php?id='.$id[$i].'&url='.$url[$i].'&name='.$name[$i].'&effect='.$effect[$i].'&quot;)">修改</a></td>';
 						echo '<td><a href="delete.php?id='.$id[$i].'">刪除</td>';
-						echo '</tr>';
-
-				}
-
-		?>
-	</table>
-	<h1 align="center">新增覺醒技能資料</h1>
-	<form action="create.php" method="post">
-	  <table width="800" border="1" bgcolor="#cccccc" align="center">
-		<tr>
-		  <th>id</th>
-		  <td bgcolor="#FFFFFF"><input type="text" name="id" /></td>
-		</tr>
-		<tr>
-		  <th>圖片url</th>
-		  <td bgcolor="#FFFFFF"><input type="text" name="url"  /></td>
-		</tr>
-		<tr>
-		  <th>名稱</th>
-		  <td bgcolor="#FFFFFF"><input type="text" name="name" /></td>
-		</tr>
-		<tr>
-		  <th>效果</th>
-		  <td bgcolor="#FFFFFF"><input type="text" name="effect" /></td>
-		</tr>
-		<tr>
-		  <th colspan="2"><input type="submit" value="新增"/></th>
-		</tr>
-	  </table>
-	</form>
+						echo '</tr>';}
+				?>
+			</table>
+			<h1 align="center">新增覺醒技能資料</h1>
+			<form action="create.php" method="post">
+	  			<table width="800" border="1" bgcolor="#cccccc" align="center">
+					<tr>
+		  				<th>id</th>
+		  				<td bgcolor="#FFFFFF"><input type="text" name="id" /></td>
+					</tr>
+					<tr>
+		  				<th>圖片url</th>
+		  				<td bgcolor="#FFFFFF"><input type="text" name="url"  /></td>
+					</tr>
+					<tr>
+		  				<th>名稱</th>
+		  				<td bgcolor="#FFFFFF"><input type="text" name="name" /></td>
+					</tr>
+					<tr>
+		  				<th>效果</th>
+		  				<td bgcolor="#FFFFFF"><input type="text" name="effect" /></td>
+					</tr>
+					<tr>
+		  				<th colspan="2"><input type="submit" value="新增"/></th>
+					</tr>
+	  			</table>
+			</form>
+		</div>
+    </div>
+    <footer>
+        <p>&copy;2015, 劉慈恩、鄭瑜、林筠傑</p>
+    </footer>
 </body>
-
 </html>
