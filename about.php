5f32e965158dd4ab0b7f4aa7a81c6eeb45b7b358
Hane Liu <hane0131@gmail.com>
2015-06-30 22:47:10 +0800

    Initial commit

---
+++ about.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>龍族拼圖寵物查詢系統</title>
+    <link rel="stylesheet" type="text/css" href="assets/stylesheets/main.css">
+    <script type="text/javascript" src="assets/scripts/main.js"></script>
+</head>
+<body>
+    <header>
+        <div class="wrapper">
+            <a id="title" href="./">龍族拼圖寵物查詢系統</a>
+            <nav>
+                <ul>
+                    <li><a href="./">Home</a></li>
+                    <li><a href="about.php">About</a></li>
+                    <li><a href="search.php">Search</a></li>
+                </ul>
+            </nav>
+            <a id="dashboard" href="login.php">Dashboard</a>
+        </div>
+    </header>
+    <div id="main">
+        <div class="wrapper">
+            <h1>關於我們</h1>
+            <h2>簡介</h2>
+            <p>我們的想法來自以下網站<a href="http://pad.skyozora.com">PAD戰友網</a>，其中的寵物圖鑑查詢功能，因為常常會有流量過大的問題，所以想做一個獨立的查詢系統，並且設有新增與修改資料的功能，而此功能只有管理員登入之後才能使用，可避免一般訪客惡意竄改資料，誤導玩家。</p>
+            <h2>系統目標</h2>
+            <ol>
+                <li>提供使用者一個方便檢索的頁面，可以經由使用者設定限制條件後，找到需要的所有寵物。</li>
+                <li>管理員可以經由帳號密碼登入後，對現有資料作新增/修改等功能。</li>
+            </ol>
+            <h2>主要分工</h2>
+            <ul>
+                <li>劉慈恩 : 排版設計、登入控制</li>
+                <li>鄭瑜 : 後端控制</li>
+                <li>林筠傑 : 維護資料庫</li>
+            </ul>
+        </div>
+    </div>
+    <footer>
+        <p>&copy;2015, 劉慈恩、鄭瑜、林筠傑</p>
+    </footer>
+</body>
+</html>
