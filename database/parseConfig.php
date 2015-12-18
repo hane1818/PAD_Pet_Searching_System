5d4f4f40f1e6e29b1fc678d76a7b9cb6ef7fa64f
Hane Liu <hane0131@gmail.com>
2015-12-19 01:13:51 +0800

    fix environment variable get error

--- database/parseConfig.php
+++ database/parseConfig.php
@@ -2,9 +2,9 @@
 
 class parseConfig{
 
-    const APPID = $_ENV['APPID'];
-    const MASTERKEY = $_ENV['MASTER_KEY'];
-    const RESTKEY = $_ENV['REST_KEY'];
+    const APPID = getenv('APPID');
+    const MASTERKEY = getenv('MASTER_KEY');
+    const RESTKEY = getenv('REST_KEY');
     const PARSEURL = 'https://api.parse.com/1/';
 }
 
