e5061e704e3de51aaad528b00987a19caf67dcb4
Hane Liu <hane0131@gmail.com>
2015-12-19 01:22:33 +0800

    fix error

--- database/parseConfig.php
+++ database/parseConfig.php
@@ -1,10 +1,13 @@
 <?php
 
 class parseConfig{
+    private $appid_ = getenv('APPID');
+    private $masterkey_ = getenv('MASTER_KEY');
+    private $restkey_ = getenv('REST_KEY');
 
-    const APPID = getenv('APPID');
-    const MASTERKEY = getenv('MASTER_KEY');
-    const RESTKEY = getenv('REST_KEY');
+    const APPID = $appid_;
+    const MASTERKEY = $masterkey_;
+    const RESTKEY = $restkey_;
     const PARSEURL = 'https://api.parse.com/1/';
 }
 
