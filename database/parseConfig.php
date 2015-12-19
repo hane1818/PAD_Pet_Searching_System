71b3802eed87386b649b0be51f737bd793625a71
Hane Liu <hane0131@gmail.com>
2015-12-19 15:10:28 +0800

    try origin string

--- database/parseConfig.php
+++ database/parseConfig.php
@@ -1,13 +1,10 @@
 <?php
 
 class parseConfig{
-    private $appid_ = getenv('APPID');
-    private $masterkey_ = getenv('MASTER_KEY');
-    private $restkey_ = getenv('REST_KEY');
 
-    const APPID = constant($appid_);
-    const MASTERKEY = constant($masterkey_);
-    const RESTKEY = constant($restkey_);
+    const APPID = 'Hyjv51T90Vd9FGUAxUcpko31zYiBMcg9aIZoppTO';
+    const MASTERKEY = 'eE6Uxf7SwnMvzbJH3o5gO4eBSWlZBAfQbw9C5uwB';
+    const RESTKEY = 'bIeQBmpQZD5EqdCV2cacJjorLJXVObXO5Ct8QL3b';
     const PARSEURL = 'https://api.parse.com/1/';
 }
 
