631c44de969d6587cd659efb285daeb6b064d99d
Hane Liu <hane0131@gmail.com>
2016-01-31 13:31:17 +0800

    fix error

--- database.php
+++ database.php
@@ -3,7 +3,7 @@ $myfile = fopen("pad-sqlite.sql", "r");
 $sql = fread($myfile, filesize("pad-sqlite.sql"));
 fclose($myfile);
 
-$db = new sqlite3('pad.db');
+$db = new SQLite3('pad.db');
 $db->exec($sql);
 echo $db->exec('SELECT * FROM wakeskill');
  ?>
